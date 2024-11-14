b1e2ab40303e76cf09b2ca6936881328ed2bd7a9
Akira-uestc <akira.uestc@gmail.com>
2024-11-14 23:25:40 +0800

    fix: resolve failure in capturing shutdown signal.

---
+++ README.md
@@ -0,0 +1,4 @@
+## Intro
+
+A tool that allows me to save Hyprland windows layout when system shutdown down,
+and restore them when power on next time.
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
-mod sig;
-mod save;
 mod reload;
+mod save;
+mod sig;
 
-use sig::signal_shutdown;
+use signal_hook::consts::signal::*;
+use signal_hook::flag as signal_flag;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
 
-fn main() {
-    crate::reload::restore_window();
-    loop {
-        if signal_shutdown() {
-            crate::save::save_layout();
+fn main() -> std::io::Result<()> {
+    let term_flag = Arc::new(AtomicBool::new(false));
+    let term_flag_clone = Arc::clone(&term_flag);
+
+    signal_flag::register(SIGTERM, term_flag.clone()).expect("Failed to register SIGTERM handler");
+    signal_flag::register(SIGINT, term_flag.clone()).expect("Failed to register SIGINT handler");
+
+    let handle = thread::spawn(move || {
+        while !term_flag_clone.load(Ordering::Relaxed) {
+            println!("Working...");
+            thread::sleep(Duration::from_secs(1));
         }
-    }
+
+        crate::save::save_layout();
+    });
+
+    handle.join().expect("Failed to join the thread");
+
+    Ok(())
 }
